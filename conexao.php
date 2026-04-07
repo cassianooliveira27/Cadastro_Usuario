26011b4c9b301ddffd088912900d9b7ebac081fc
cassianooliveira27 <cassianooliveira308@gmail.com>
2026-04-07 19:50:52 -0300

    Criação de um arquivo conexao.php contendo a conexão com o banco de dados utilizando PDO

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$db   = "cadastro_db";
+$user = "root";
+$pass = "";
+$charset = "utf8";
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro de conexão: " . $e->getMessage());
+}
+?>
